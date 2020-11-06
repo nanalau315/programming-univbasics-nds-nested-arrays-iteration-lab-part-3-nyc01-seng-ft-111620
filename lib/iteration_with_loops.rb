eaf39c8a31474ddf6dc7cce0e17e5db4abd70d0c
Nana Lau <nanalau315@hotmail.com>
2020-11-06 02:39:42 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,4 +3,28 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   
   
+  
+end
+
+
+
+
+
+
+def find_even_values(src)
+ 
+  
+  counter = 0
+  while counter < src.length do
+    inner_counter = 0
+    while inner_counter < src[counter].length do
+      if src[counter][inner_counter].even?
+        p src[counter][inner_counter]
+        
+      end
+      inner_counter += 1
+    end
+    counter += 1
+  end
+
 end
