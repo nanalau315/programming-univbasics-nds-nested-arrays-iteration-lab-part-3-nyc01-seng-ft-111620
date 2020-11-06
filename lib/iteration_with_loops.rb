cf07e6835a0bc181095d68a038516f0e89ec6cc7
Nana Lau <nanalau315@hotmail.com>
2020-11-06 02:42:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -15,31 +15,6 @@ def join_nested_strings(src)
     end
     counter += 1
   end
-
+ new_arr.join(" ")
 end
   
-  
-end
-
-
-
-
-
-
-def find_even_values(src)
- 
-  
-  counter = 0
-  while counter < src.length do
-    inner_counter = 0
-    while inner_counter < src[counter].length do
-      if src[counter][inner_counter].even?
-        p src[counter][inner_counter]
-        
-      end
-      inner_counter += 1
-    end
-    counter += 1
-  end
-
-end
